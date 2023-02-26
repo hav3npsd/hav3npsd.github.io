c0ae18470d65ea7c3bd932782a4856ffb22f3c9a
nnabse <nnabse@gmail.com>
2023-02-26 17:59:59 +0300

    add main page structure, add mtdef and rounded class, add variables file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MainComponent } from '@pages/main/main.component';
 import { MainModule } from '@pages/main/main.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainComponent],
   imports: [BrowserModule, AppRoutingModule, MainModule],
   providers: [],
   bootstrap: [AppComponent],
