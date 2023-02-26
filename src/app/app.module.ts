f9b6acd1afdc234261cffe445a35d3ff728eb8cf
nnabse <nnabse@gmail.com>
2023-02-26 16:48:43 +0300

    add main module, setted up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MainModule } from '@pages/main/main.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MainModule],
   providers: [],
   bootstrap: [AppComponent],
 })
