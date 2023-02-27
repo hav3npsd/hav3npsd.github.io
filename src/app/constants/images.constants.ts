83712d11fa61eaabd2d05a0366747b042edb7487
nnabse <nnabse@gmail.com>
2023-02-27 19:36:01 +0300

    fill work component, add razoomart logotypes, memoji images, add font weight, fix constant name

--- src/app/constants/images.constants.ts
+++ src/app/constants/images.constants.ts
@@ -2,11 +2,31 @@ import { ImageInfo } from '@interfaces/imageInfo.interfaces';
 
 enum ImageType {
   ilyaK = 'ilyaK',
+  ilyaU = 'ilyaU',
+  artem = 'artem',
+  albina = 'albina',
+  maksim = 'maksim',
 }
 
 export const ImageList: { [key in ImageType]: ImageInfo } = {
   [ImageType.ilyaK]: {
     link: 'assets/images/memojis/ilyaK.webp',
+    description: 'memoji icon',
+  },
+  [ImageType.ilyaU]: {
+    link: 'assets/images/memojis/ilyaU.webp',
+    description: 'memoji icon',
+  },
+  [ImageType.artem]: {
+    link: 'assets/images/memojis/artem.webp',
+    description: 'memoji icon',
+  },
+  [ImageType.albina]: {
+    link: 'assets/images/memojis/albina.webp',
+    description: 'memoji icon',
+  },
+  [ImageType.maksim]: {
+    link: 'assets/images/memojis/maksim.webp',
     description: 'memoji icon',
   },
 };
