556f28b9090422f66b870355b581865a6b0fe9ca
nnabse <nnabse@gmail.com>
2023-04-23 11:57:26 +0300

    add header home routing feature

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FooterComponent } from '@shared/footer/footer.component';
 import { HeaderComponent } from '@shared/header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
