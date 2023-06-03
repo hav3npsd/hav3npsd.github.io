255fb5063d9d23453f4b34ce5d503116c5d6de9f
nnabse <nnabse@gmail.com>
2023-06-03 15:09:10 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hav3npsd-root',
+  selector: 'hav3npsd',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'hav3npsd';
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { AppModule } from '@hav3npsd/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
