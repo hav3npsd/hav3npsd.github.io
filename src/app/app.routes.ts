9628913b84877c4032d92ff9530b46edb35e61b3
nnabse <nnabse@gmail.com>
2023-06-03 15:44:04 +0700

    add app and main module routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@pages/main/main.module').then((m) => m.MainModule),
+  },
+];
